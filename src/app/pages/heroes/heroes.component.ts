5c8097e6e1ce9d9f3a94fa364568ef203dd04469
Wolf08 <alejandro.montecillo@gmail.com>
2021-11-09 13:17:07 -0600

    :sparkles: Get data from database on  FIREBASE

--- src/app/pages/heroes/heroes.component.ts
+++ src/app/pages/heroes/heroes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroeModel } from 'src/app/models/heroe.model';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-heroes',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-
-  constructor() { }
+  
+  heroes: HeroeModel[] = [];
+  constructor(private heroesService: HeroesService) { }
 
   ngOnInit(): void {
+    //obtenccion de los heroes 
+    this.heroesService.getHeroes()
+    .subscribe(respuesta =>{
+      console.log(respuesta);
+      this.heroes = respuesta;
+    })
   }
 
 }
--- src/app/services/heroes.service.ts
+++ src/app/services/heroes.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient,  } from '@angular/common/http';
 import { HeroeModel } from '../models/heroe.model';
 import {map } from 'rxjs/operators'
+import { ObjectUnsubscribedError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -35,6 +36,31 @@ export class HeroesService {
     //Elimicacion del ID duplicado
     delete heroeTemp.id
     return this.http.put(`${this.url}/heroes/${heroe.id}.json`,heroeTemp)
+
+  }
+
+  getHeroes(){
+
+    return this.http.get(`${this.url}/heroes.json`)
+    .pipe(
+      map( this.crearArrayHeroes) //Modificacion de la respuesta para convertirla en un array iterable
+    )
+  }
+
+  private crearArrayHeroes( heroesObj:   any){
+    //Crear el tipo de dato de heroe y array vacio
+    const heroes: HeroeModel[] = [];
+    //Recorrer el objeto por llaves like python
+
+    if( heroesObj === null ) { return []; }
+    Object.keys( heroesObj ).forEach( key => {
+      const heroe: HeroeModel = heroesObj[key];
+      heroe.id = key;
+      heroes.push(heroe);
+    });
+    return heroes;
+
+   
 
   }
 
